244a28c4471c2b7adcfd4c20af6816cbcd75f8f4
Nstampfli <nathan.stampfli@hetic.net>
2020-01-07 11:26:36 +0100

    remove obsctacle <0 and invincible

--- src/js/script.js
+++ src/js/script.js
@@ -29,14 +29,17 @@ let invincible = false;
 function hitbox() {
   for (let i = 0; i < obstacles.length; i++) {
     if (
-      invincible == false &&
+      !invincible &&
       obstacles[i].y <= manY + man.height &&
       manX + man.width >= obstacles[i].x &&
       manX <= obstacles[i].x + cars.width
     ) {
       console.log("lol");
       invincible = true;
-      setTimeout((invincible = false), 1500);
+      setTimeout(() => (invincible = false), 1500);
+    } else if (obstacles[i].x + cars.width <= 0) {
+      obstacles.shift(i, 1);
+      console.log(obstacles.length);
     }
   }
 }
@@ -56,16 +59,11 @@ function obs() {
 }
 function jump(event) {
   switch (event.key) {
-    case "ArrowUp":
+    case "z":
       if (manY === 400) {
         manY -= 150;
       }
       break;
-    /*case "ArrowLeft":
-      if (monsterX > 40) {
-        monsterX -= direction;
-      }
-      break;*/
     default:
       break;
   }
